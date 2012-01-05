6d06df2c6a4bd2de0aa1205efdd4f488bd57ed02
Dave Gerrard <davetgerrard@gmail.com>
2012-01-05 17:05:20 +0000

    attempting to make sweave template from README

--- makePackageSkeleton.R
+++ makePackageSkeleton.R
@@ -1,8 +1,19 @@
 library(roxygen2)
 
-setwd("C:/Users/dave/HalfStarted/mlgt")
+#setwd("C:/Users/dave/HalfStarted/mlgt")
+setwd("C:/Users/dave/HalfStarted/mlgt/testRoxygen")
 
 package.skeleton(name = "mlgt", code_files="mlgt.R", force=TRUE)
 
 roxygenize("mlgt", unlink.target=T)
+
+
+
+
+### to use sweave on documentation
+# in R
+Sweave("testSweave.R")
+# then in DOS.
+R CMD texify --pdf testSweave.R.tex
+
 
