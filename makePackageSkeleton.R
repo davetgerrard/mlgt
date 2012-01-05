b883993a35b209914bbd25f6f8732d92633b8429
Dave Gerrard <davetgerrard@gmail.com>
2012-01-05 09:11:13 +0000

    Testing roxygen on newly formed package

--- makePackageSkeleton.R
+++ makePackageSkeleton.R
@@ -1,3 +1,8 @@
+library(roxygen2)
+
 setwd("C:/Users/dave/HalfStarted/mlgt")
 
-package.skeleton(name = "mlgt", code_files="mlgt.R")
+package.skeleton(name = "mlgt", code_files="mlgt.R", force=TRUE)
+
+roxygenize("mlgt", unlink.target=T)
+
